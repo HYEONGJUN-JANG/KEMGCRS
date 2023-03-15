75115147f2a2d2d689d427e6aa86ff77182205d5
hj <hjhj>
2023-03-15 13:10:23 +0900

    Retriever class 분리 (Models.py) eval_know 로 기존의 know_text topk 뽑아저장하던 코드 분리

--- main.py
+++ main.py
@@ -10,22 +10,7 @@ import data
 from config import bert_special_tokens_dict
 from dataModel import KnowledgeDataset
 from utils import *
-
-
-class Retriever(nn.Module):
-    def __init__(self, bert_model, hidden_size):
-        super(Retriever, self).__init__()
-        self.bert_model = bert_model
-        self.proj = nn.Sequential(
-            nn.Linear(hidden_size, hidden_size // 2),
-            nn.ReLU(),
-            nn.Linear(hidden_size // 2, hidden_size)
-        )
-
-    def forward(self, token_seq, mask):
-        dialog_emb = self.bert_model(input_ids=token_seq, attention_mask=mask).last_hidden_state[:, 0, :]  # [B, d]
-        dialog_emb = self.proj(dialog_emb)
-        return dialog_emb
+from models import *
 
 
 def knowledge_reindexing(args, knowledge_data, bert_model):
@@ -76,17 +61,53 @@ def train(args, train_dataloader, knowledge_data, bert_model):
             loss.backward()
             optimizer.step()
         print('LOSS:\t%.4f' % total_loss)
-
     torch.save(bert_model.state_dict(), f"{args.time}_{args.model_name}_bin.pt")  # TIME_MODELNAME 형식
 
+def eval_know(args, test_dataloader, bert_model, tokenizer, knowledge_index):
+
+    # Read knowledge DB
+    knowledgeDB = data.read_pkl(os.path.join(args.data_dir, args.k_DB_name))  # TODO: verbalize (TH)
+    knowledge_data = KnowledgeDataset(args, knowledgeDB, tokenizer)  # knowledge dataset class
+    knowledge_index = knowledge_index.to(args.device)
+    jsonlineSave = []
+    bert_model = bert_model.to(args.device)
+
+    cnt = 0
+    for batch in tqdm(test_dataloader, desc="Knowledge_Test", bar_format=' {percentage:3.0f} % | {bar:23} {r_bar}'): # TODO: Knowledge task 분리중
+        batch_size = batch[0].size(0)
+        dialog_token = batch[0].to(args.device)
+        dialog_mask = batch[1].to(args.device)
+        target_knowledge = batch[2].to(args.device)
+        goal_type = batch[3]  #
+        response = batch[4]
+        topic = batch[5]
+
+        # dialog_emb = retriever(dialog_token, dialog_mask)  # [B, d]
+        dialog_emb = bert_model(input_ids=dialog_token, attention_mask=dialog_mask).last_hidden_state[:, 0, :]  # [B, d]
+        dot_score = torch.matmul(dialog_emb, knowledge_index.transpose(1, 0))  # [B, N]
+
+        top_candidate = torch.topk(dot_score, k=args.know_topk, dim=1).indices  # [B, K]
+
+        input_text = '||'.join(tokenizer.batch_decode(dialog_token, skip_special_tokens=True))
+        target_knowledge_text = [knowledgeDB[idx] for idx in target_knowledge]  # target knowledge
+        retrieved_knowledge_text = [knowledgeDB[idx] for idx in top_candidate[0]]  # list
+        correct = target_knowledge_text[0] in retrieved_knowledge_text
+
+        jsonlineSave.append({'goal_type': goal_type[0], 'topic': topic, 'tf': correct, 'dialog': input_text, 'target': '||'.join(target_knowledge_text), 'response': response[0], "predict5": retrieved_knowledge_text})
+        cnt += 1
+
+    # TODO HJ: 입출력 저장 args처리 필요시 args.save_know_output 에 store_true 옵션으로 만들 필요
+    write_pkl(obj=jsonlineSave, filename='jsonline.pkl')  # 입출력 저장
+    save_json(args, f"{args.time}_inout", jsonlineSave)
+
+    print('done')
 
 def main():
     args = parseargs()
-    args.data_cache = False
+    # args.data_cache = False
     checkPath(args.log_dir)
     logging.basicConfig(level=logging.DEBUG, filename=os.path.join(args.log_dir, f'{args.time}_{args.log_name + "_"}log.txt'), filemode='a', format='%(asctime)s: %(levelname)s: %(message)s', datefmt='%Y/%m/%d_%p_%I:%M:%S ')
     logging.info('Commend: {}'.format(', '.join(map(str, sys.argv))))
-    args.device = f'cuda:{args.device}' if args.device else "cpu"
 
     # Model cached load
     checkPath(os.path.join("cache", args.model_name))
@@ -105,51 +126,13 @@ def main():
 
     knowledge_index = knowledge_index.to(args.device)
 
-    jsonlineSave = []
     if args.model_load:
         bert_model.load_state_dict(torch.load(os.path.join(args.model_dir, args.pretrained_model)))  # state_dict를 불러 온 후, 모델에 저장`
     bert_model = bert_model.to(args.device)
 
     train(args, train_dataloader, knowledge_data, bert_model)  # [TH] <topic> 추가됐으니까 재학습
+    eval_know(args, test_dataloader, bert_model, tokenizer, knowledge_index) # HJ: Knowledge text top-k 뽑아서 output만들어 체크하던 코드 분리
 
-    cnt = 0
-    for batch in tqdm(test_dataloader, desc="Main_Test", bar_format=' {percentage:3.0f} % | {bar:23} {r_bar}'): # TODO: 분리해야 할듯 (예: evaluation.py) (TH)
-        batch_size = batch[0].size(0)
-        dialog_token = batch[0].to(args.device)
-        dialog_mask = batch[1].to(args.device)
-        target_knowledge = batch[2].to(args.device)
-        goal_type = batch[3]  #
-        response = batch[4]
-        topic = batch[5]
-
-        # dialog_emb = retriever(dialog_token, dialog_mask)  # [B, d]
-        dialog_emb = bert_model(input_ids=dialog_token, attention_mask=dialog_mask).last_hidden_state[:, 0, :]  # [B, d]
-        dot_score = torch.matmul(dialog_emb, knowledge_index.transpose(1, 0))  # [B, N]
-
-        top_candidate = torch.topk(dot_score, k=args.know_topk, dim=1).indices  # [B, K]
-
-        # 10개 추리고, target가 가까운 5개
-        # top_candidate_emb = knowledge_index[top_candidate]  # [B, K, d]
-        # target_knowledge_emb = knowledge_index[target_knowledge]  # [B, d]
-        # next_hop_prob = torch.sum(top_candidate_emb * target_knowledge_emb.unsqueeze(1), dim=2)  # [B, K]
-        # next_hop_index = torch.topk(next_hop_prob, k=5, dim=1).indices  # [B, 1]
-        # next_hop = top_candidate[torch.arange(batch_size), next_hop_index.squeeze(1)]
-
-        input_text = '||'.join(tokenizer.batch_decode(dialog_token, skip_special_tokens=True))
-        target_knowledge_text = [knowledgeDB[idx] for idx in target_knowledge]  # target knowledge
-        retrieved_knowledge_text = [knowledgeDB[idx] for idx in top_candidate[0]]  # list
-        correct = target_knowledge_text[0] in retrieved_knowledge_text
-
-        jsonlineSave.append({'goal_type': goal_type[0], 'topic': topic, 'tf': correct, 'dialog': input_text, 'target': '||'.join(target_knowledge_text), 'response': response[0], "predict5": retrieved_knowledge_text})
-        cnt += 1
-        # if cnt == 22: break
-        # correct.append((target_knowledge_text == retrieved_knowledge_text))
-
-    # TODO 입출력 저장
-    write_pkl(obj=jsonlineSave, filename='jsonline.pkl')  # 입출력 저장
-    save_json(args, f"{args.time}_inout", jsonlineSave)
-
-    print('done')
 
 
 if __name__ == "__main__":
