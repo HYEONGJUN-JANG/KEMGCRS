55aef65a893d53b1f7aba64291ad3a7de52b04cd
taeho <hirooms2@hanyang.ac.kr>
2023-03-30 11:20:36 +0900

    230328 BART버전 generation

--- TH_new.py
+++ TH_new.py
@@ -225,6 +225,17 @@ class Retriever(nn.Module):
         outputs = self.query_bert(input_ids=token_seq, attention_mask=mask, labels=labels, output_hidden_states=True)
         return outputs[0]
 
+    def compute_know_score(self, token_seq, mask, knowledge_index):
+        """
+        eval_know.computing_score에서
+        모든 key vector에서 올라온 벡터를 통해 계산처리
+        """
+        if self.args.usebart: dialog_emb = self.query_bert(input_ids=token_seq, attention_mask=mask, output_hidden_states=True).decoder_hidden_states[-1][:,0,:].squeeze(1)
+        else: dialog_emb = self.query_bert(input_ids=token_seq, attention_mask=mask).last_hidden_state[:, 0, :]  # [B, d]
+        # dialog_emb = self.query_bert(input_ids=token_seq, attention_mask=mask).last_hidden_state[:, 0, :]  # [B, d]
+        dot_score = torch.matmul(dialog_emb, knowledge_index.transpose(1, 0))  # [B, N]
+        return dot_score
+
     def knowledge_retrieve(self, token_seq, mask, candidate_knowledge_token, candidate_knowledge_mask):
         """
         Args: 뽑아준 negative에 대해서만 dot-product
@@ -323,6 +334,69 @@ class DialogDataset(Dataset):  # knowledge용 데이터셋
 
     def __len__(self):
         return len(self.augmented_raw_sample)
+
+
+def knowledge_reindexing(args, knowledge_data, retriever):
+    # 모든 know_index를 버트에 태움
+    print('...knowledge indexing...')
+    knowledgeDataLoader = DataLoader(
+        knowledge_data,
+        batch_size=args.batch_size
+    )
+    knowledge_index = []
+
+    for batch in tqdm(knowledgeDataLoader):
+        input_ids = batch[0].to(args.device)
+        attention_mask = batch[1].to(args.device)
+        knowledge_emb = retriever.query_bert(input_ids=input_ids, attention_mask=attention_mask, output_hidden_states=True).decoder_hidden_states[-1][:, 0, :].squeeze(1)
+        knowledge_index.extend(knowledge_emb.cpu().detach())
+    knowledge_index = torch.stack(knowledge_index, 0)
+    return knowledge_index
+
+
+def eval_know(args, test_dataloader, retriever, knowledge_data, knowledgeDB, tokenizer):
+
+    # Read knowledge DB
+    # knowledge_index = knowledge_reindexing(args, knowledge_data, retriever)
+    # knowledge_index = knowledge_index.to(args.device)
+    jsonlineSave = []
+    # bert_model = bert_model.to(args.device)
+    knowledge_index = knowledge_reindexing(args, knowledge_data, retriever)
+    knowledge_index = knowledge_index.to(args.device)
+
+    cnt = 0
+    for batch in tqdm(test_dataloader, desc="Knowledge_Test", bar_format=' {percentage:3.0f} % | {bar:23} {r_bar}'): # TODO: Knowledge task 분리중
+        dialog_token = batch['dialog_token']
+        dialog_mask = batch['dialog_mask']
+        response = batch['response']
+        candidate_knowledge_token = batch['candidate_knowledge_token']  # [B,5,256]
+        type_idx = batch['type']
+        topic_idx = batch['topic_idx']
+        candidate_knowledge_mask = batch['candidate_knowledge_mask']  # [B,5,256]
+        target_knowledge = candidate_knowledge_token[:, 0, :]
+
+        # tokenizer.batch_decode(dialog_token, skip_special_tokens=True)  # 'dialog context'
+        # print([knowledgeDB[idx] for idx in target_knowledge]) # target knowledge
+        # dot_score = retriever.compute_score(response_token, response_mask, knowledge_index)
+        dot_score = retriever.compute_know_score(dialog_token, dialog_mask, knowledge_index)
+        top_candidate = torch.topk(dot_score, k=args.know_topk, dim=1).indices  # [B, K]
+
+        input_text = '||'.join(tokenizer.batch_decode(dialog_token, skip_special_tokens=True))
+        target_knowledge_text = tokenizer.batch_decode(target_knowledge, skip_special_tokens=True)  # target knowledge
+        retrieved_knowledge_text = [knowledgeDB[idx] for idx in top_candidate[0]]  # list
+        correct = target_knowledge_text in retrieved_knowledge_text
+        response = '||'.join(tokenizer.batch_decode(response, skip_special_tokens=True))
+
+        type_idx = [args.goalDic['int'][int(idx)] for idx in type_idx]
+        topic_idx = [args.topicDic['int'][int(idx)] for idx in topic_idx]
+
+        jsonlineSave.append({'goal_type': type_idx[0], 'topic': topic_idx[0], 'tf': correct, 'dialog': input_text, 'target': '||'.join(target_knowledge_text), 'response': response, "predict5": retrieved_knowledge_text})
+        cnt += 1
+
+    # TODO HJ: 입출력 저장 args처리 필요시 args.save_know_output 에 store_true 옵션으로 만들 필요
+    write_pkl(obj=jsonlineSave, filename='jsonline.pkl')  # 입출력 저장
+    save_json(args, f"{args.time}_{args.log_name}_inout", jsonlineSave)
+    print('done')
 
 
 def main():
@@ -425,7 +499,7 @@ def main():
     train_datamodel_know = DialogDataset(args, train_dataset, knowledgeDB, tokenizer, task='know')
     test_datamodel_know = DialogDataset(args, test_dataset, knowledgeDB, tokenizer, task='know')
     train_dataloader = DataLoader(train_datamodel_know, batch_size=args.batch_size, shuffle=True)
-    test_dataloader = DataLoader(test_datamodel_know, batch_size=args.batch_size, shuffle=False)
+    test_dataloader = DataLoader(test_datamodel_know, batch_size=1, shuffle=False)
 
     for epoch in range(args.num_epochs):
         train_epoch_loss = 0
