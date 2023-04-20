745a61b5af84820fa6347a12747e63fc7ce5d8f4
taeho <hirooms2@hanyang.ac.kr>
2023-04-21 00:28:15 +0900

    230418 Knowledge Indexing

--- data_model.py
+++ data_model.py
@@ -162,15 +162,15 @@ class DialogDataset(Dataset):  # knowledge용 데이터셋
         context_batch['topic'] = self.tokenizer(topic, truncation=True, padding='max_length', max_length=32).input_ids
 
         candidate_positives_idx = candidate_positives_idx[:self.args.pseudo_pos_num]
-        sampled_pair = sorted(random.sample(list(range(len(candidate_positives_idx))), k=2))
-        pseudo_positive = candidate_positives_idx[sampled_pair[0]]
-        pseudo_negative = candidate_positives_idx[sampled_pair[1]]
+        # sampled_pair = sorted(random.sample(list(range(len(candidate_positives_idx))), k=2))
+        # pseudo_positive = candidate_positives_idx[sampled_pair[0]]
+        # pseudo_negative = candidate_positives_idx[sampled_pair[1]]
 
         # pseudo_positive = random.choice(candidate_positives_idx)
         # pseudo_positive = candidate_positives_idx[self.args.pseudo_pos_rank - 1]
         # pseudo_negative = self.negative_sampler(pseudo_positive)
 
-        candidate_indice = [pseudo_positive, pseudo_negative]
+        candidate_indice = candidate_positives_idx
         candidate_knowledge_text = [self.args.knowledgeDB[idx] for idx in candidate_indice]
         candidate_knowledge = self.tokenizer(candidate_knowledge_text, truncation=True, padding='max_length', max_length=self.args.max_length)
         candidate_knowledge_token = candidate_knowledge.input_ids
