d65cecc7cbc61f9f732b7c8e0e01ad0146da9d9a
taeho <hirooms2@hanyang.ac.kr>
2023-04-28 15:57:16 +0900

    230421 Cand2

--- train_know.py
+++ train_know.py
@@ -110,7 +110,8 @@ def train_know(args, train_dataloader, test_dataloader, retriever, knowledge_dat
 
                 Qd = (pseudo_soft_label) / torch.sum(pseudo_soft_label, dim=1, keepdim=True)
                 # kl_div = torch.sum(Pd * (Pd / Qd).log(), dim=1)
-                loss = -torch.mean(Qd * torch.log(Pd + 1e-10))
+                loss = -torch.sum(Qd * torch.log(Pd + 1e-10), dim=1)
+                loss = torch.mean(loss)
                 # loss = nn.KLDivLoss(reduction='sum')(Qd.log(), Pd)
 
                 # loss = 0
