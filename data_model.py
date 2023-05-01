7982e901b50a3228398d9952d40e0d552f0fef14
taeho <hirooms2@hanyang.ac.kr>
2023-05-01 15:23:37 +0900

    230421 Cand2

--- data_model.py
+++ data_model.py
@@ -161,9 +161,9 @@ class DialogDataset(Dataset):  # knowledge용 데이터셋
         context_batch['topic_idx'] = self.args.topicDic['str'][topic]  # index로 바꿈
         context_batch['topic'] = self.tokenizer(topic, truncation=True, padding='max_length', max_length=32).input_ids
 
-        # Positive grouping
-        candidate_knowledges = candidate_knowledges[:self.args.pseudo_pos_num]
-        candidate_confidences = candidate_confidences[:self.args.pseudo_pos_num]
+        # # Positive grouping
+        # candidate_knowledges = candidate_knowledges[:self.args.pseudo_pos_num]
+        # candidate_confidences = candidate_confidences[:self.args.pseudo_pos_num]
 
         # random_idx = random.randrange(min(self.args.pseudo_pos_num, len(candidate_knowledges)))
 
@@ -172,9 +172,9 @@ class DialogDataset(Dataset):  # knowledge용 데이터셋
         # candidate_knowledges = [candidate_knowledges[random_idx]]
         # candidate_confidences = [candidate_confidences[random_idx]]
 
-        # # Only Top-K
-        # candidate_knowledges = [candidate_knowledges[self.args.pseudo_pos_num]]
-        # candidate_confidences = [candidate_confidences[self.args.pseudo_pos_num]]
+        # Only Top-K
+        candidate_knowledges = [candidate_knowledges[self.args.pseudo_pos_num]]
+        candidate_confidences = [candidate_confidences[self.args.pseudo_pos_num]]
 
         # candidate_knowledges = candidate_knowledges + [0] * (self.args.pseudo_pos_num - len(candidate_knowledges))
         # candidate_confidences = candidate_confidences + [0] * (self.args.pseudo_pos_num - len(candidate_confidences))
