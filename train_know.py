667534719d6a650351e45dcd1c9bee3c33efce1b
taeho <hirooms2@hanyang.ac.kr>
2023-05-04 14:55:20 +0900

    230421 Cand2

--- train_know.py
+++ train_know.py
@@ -84,35 +84,35 @@ def train_know(args, train_dataloader, test_dataloader, retriever, knowledge_dat
 
                 logit = retriever.compute_know_score(dialog_token, dialog_mask, knowledge_index, goal_type)
 
-                ### Positive sampling
-                for i in range(batch['pseudo_targets'].size(1)):
-                    pseudo_mask = torch.zeros_like(logit)
-                    pseudo_mask[:, 0] = -1e10
-                    pseudo_target = batch['pseudo_targets'][:, i]  # [B]
-                    for j in range(batch['pseudo_targets'].size(1)):
-                        if j != i:
-                            exclude = batch['pseudo_targets'][:, j]
-                            pseudo_mask[torch.arange(logit.size(0)), exclude] = -1e10
-                    loss += torch.mean(criterion(logit + pseudo_mask, pseudo_target))  # For MLP predict
-
-                ### Group-wise
-                # loss = 0
+                # ### Positive sampling
                 # for i in range(batch['pseudo_targets'].size(1)):
-                #     pseudo_target_list = batch['pseudo_targets'][:, :i+1]
-                #     know_mask = (pseudo_target_list != 0)
-                #     num_know = torch.sum(know_mask, dim=1)
-                #     g_logit = torch.gather(logit, 1, pseudo_target_list) * know_mask
-                #     g_logit = torch.sum(g_logit, dim=1) / (num_know+1e-10)
-                #     # g_logit = torch.mean(torch.gather(logit, 1, batch['pseudo_targets']), dim=1)
                 #     pseudo_mask = torch.zeros_like(logit)
                 #     pseudo_mask[:, 0] = -1e10
-                #     for j in range(pseudo_target_list.size(1)):
-                #         pseudo_target = pseudo_target_list[:, j]  # [B]
-                #         pseudo_mask[torch.arange(logit.size(0)), pseudo_target] = -1e10
-                #     pseudo_mask = torch.cat([torch.zeros(pseudo_mask.size(0)).unsqueeze(1).to(args.device), pseudo_mask], dim=1)
-                #     logit = torch.cat([g_logit.unsqueeze(1), logit], dim=1)
-                #     # loss += torch.mean(criterion(logit + pseudo_mask, pseudo_target))
-                #     loss += (-torch.log_softmax(logit + pseudo_mask, dim=1).select(dim=1, index=0)).mean()
+                #     pseudo_target = batch['pseudo_targets'][:, i]  # [B]
+                #     for j in range(batch['pseudo_targets'].size(1)):
+                #         if j != i:
+                #             exclude = batch['pseudo_targets'][:, j]
+                #             pseudo_mask[torch.arange(logit.size(0)), exclude] = -1e10
+                #     loss += torch.mean(criterion(logit + pseudo_mask, pseudo_target))  # For MLP predict
+
+                ### Group-wise
+                loss = 0
+                for i in range(batch['pseudo_targets'].size(1)):
+                    pseudo_target_list = batch['pseudo_targets'][:, :i+1]
+                    know_mask = (pseudo_target_list != 0)
+                    num_know = torch.sum(know_mask, dim=1)
+                    g_logit = torch.gather(logit, 1, pseudo_target_list) * know_mask
+                    g_logit = torch.sum(g_logit, dim=1) / (num_know+1e-10)
+                    # g_logit = torch.mean(torch.gather(logit, 1, batch['pseudo_targets']), dim=1)
+                    pseudo_mask = torch.zeros_like(logit)
+                    pseudo_mask[:, 0] = -1e10
+                    for j in range(pseudo_target_list.size(1)):
+                        pseudo_target = pseudo_target_list[:, j]  # [B]
+                        pseudo_mask[torch.arange(logit.size(0)), pseudo_target] = -1e10
+                    pseudo_mask = torch.cat([torch.zeros(pseudo_mask.size(0)).unsqueeze(1).to(args.device), pseudo_mask], dim=1)
+                    logit = torch.cat([g_logit.unsqueeze(1), logit], dim=1)
+                    # loss += torch.mean(criterion(logit + pseudo_mask, pseudo_target))
+                    loss += (-torch.log_softmax(logit + pseudo_mask, dim=1).select(dim=1, index=0)).mean()
 
                 ### ListNet
                 # pseudo_mask = torch.zeros_like(logit)
