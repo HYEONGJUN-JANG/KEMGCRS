1a164352e595e22c8682c60da4758a30dbcb7fa7
taeho <hirooms2@hanyang.ac.kr>
2023-05-05 20:25:04 +0900

    230421 Cand2

--- train_know.py
+++ train_know.py
@@ -152,11 +152,11 @@ def train_know(args, train_dataloader, test_dataloader, retriever, knowledge_dat
                 # logit = logit + pseudo_mask
                 logit = retriever.compute_know_score_candidate(dialog_token, dialog_mask, knowledge_index[batch['candidate_indice']])
                 logit_exp = torch.exp(logit - torch.max(logit, dim=1, keepdim=True)[0])  # [B, K]
-                # pseudo_logit = torch.gather(logit_exp, 1, batch['pseudo_targets'])  # [B, K]
+                pseudo_logit = logit_exp[:, :args.pseudo_pos_num] #  torch.gather(logit_exp, 1, batch['pseudo_targets'])  # [B, K]
                 all_sum = torch.sum(logit_exp, dim=1, keepdim=True)  # [B, 1]
                 cumsum_logit = torch.cumsum(logit_exp, dim=1)  # [B, K]
                 denominator = all_sum - (cumsum_logit - logit_exp) + 1e-10
-                loss = torch.mean(torch.sum(-torch.log(logit_exp / denominator), dim=1))
+                loss = torch.mean(torch.sum(-torch.log(pseudo_logit / denominator), dim=1))
 
                 ### ListMLE2
                 # loss = 0
