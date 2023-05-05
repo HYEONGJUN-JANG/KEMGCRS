81786421df0488191e5da7138d67b52296928716
taeho <hirooms2@hanyang.ac.kr>
2023-05-05 12:03:59 +0900

    230421 Cand2

--- train_know.py
+++ train_know.py
@@ -133,7 +133,7 @@ def train_know(args, train_dataloader, test_dataloader, retriever, knowledge_dat
                 pseudo_soft_label = torch.zeros_like(logit)
                 for j in range(batch['pseudo_targets'].size(1)):
                     pseudo_soft_label[torch.arange(logit.size(0)), batch['pseudo_targets'][:, j]] = batch['pseudo_confidences'][:, j]
-                Qd = torch.softmax(pseudo_soft_label + pseudo_mask, dim=1)
+                Qd = torch.softmax(pseudo_soft_label / args.tau + pseudo_mask, dim=1)
                 loss = torch.mean(-torch.sum(Qd * torch.log(Pd + 1e-10), dim=1))
 
                 ### ListMLE
