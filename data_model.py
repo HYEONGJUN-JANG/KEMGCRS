2f4f8207066590be3a00c572fddf290f2e6eecfa
taeho <hirooms2@hanyang.ac.kr>
2023-05-06 13:40:57 +0900

    230421 Cand2

--- data_model.py
+++ data_model.py
@@ -172,9 +172,9 @@ class DialogDataset(Dataset):  # knowledge용 데이터셋
         ### Positive sampling (Top-K)
         # candidate_knowledges = candidate_knowledges[:self.args.pseudo_pos_num]
         # candidate_confidences = candidate_confidences[:self.args.pseudo_pos_num]
-        random_idx = random.randrange(self.args.pseudo_pos_num)
-        candidate_knowledges = [candidate_knowledges[random_idx]]
-        candidate_confidences = [candidate_confidences[random_idx]]
+        # random_idx = random.randrange(self.args.pseudo_pos_num)
+        # candidate_knowledges = [candidate_knowledges[random_idx]]
+        # candidate_confidences = [candidate_confidences[random_idx]]
 
         ### Positive sampling (pseudo)
         # pseudo_candidate_knowledges = [know for know, conf in zip(candidate_knowledges, candidate_confidences) if conf > 0.1]
