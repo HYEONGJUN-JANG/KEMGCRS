265b9cf7cff7cad1414c04cee623fc042762996e
taeho <hirooms2@hanyang.ac.kr>
2023-05-08 01:01:41 +0900

    230421 Cand2

--- train_know.py
+++ train_know.py
@@ -150,8 +150,7 @@ def train_know(args, train_dataloader, test_dataloader, retriever, knowledge_dat
                 # logit = logit + pseudo_mask
 
                 ### ListMLE for reranking
-                # if args.stage == 'rerank':
-                # logit = retriever.compute_know_score_candidate(dialog_token, dialog_mask, knowledge_index[batch['candidate_indice']])
+
                 logit_exp = torch.exp(logit - torch.max(logit, dim=1, keepdim=True)[0])  # [B, K]
                 pseudo_logit = logit_exp[:, :args.pseudo_pos_num]  # torch.gather(logit_exp, 1, batch['pseudo_targets'])  # [B, K]
                 all_sum = torch.sum(logit_exp, dim=1, keepdim=True)  # [B, 1]
