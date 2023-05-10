0d5004220468e40d25474551ce018d323de8d51a
taeho <hirooms2@hanyang.ac.kr>
2023-05-10 15:06:25 +0900

    230421 Cand2

--- train_know.py
+++ train_know.py
@@ -127,15 +127,15 @@ def train_know(args, train_dataloader, test_dataloader, retriever, knowledge_dat
                 # loss += (-torch.log_softmax(g_logit + pseudo_mask, dim=1).select(dim=1, index=0)).mean()
 
                 ### ListNet (final)
-                pseudo_mask = torch.zeros_like(logit)
-                pseudo_mask[:, 0] = -1e10
-                Pd = torch.softmax(logit + pseudo_mask, dim=1)
-                pseudo_soft_label = torch.zeros_like(logit) - 1e10
-                for j in range(batch['pseudo_targets'].size(1)):
-                    pseudo_soft_label[torch.arange(logit.size(0)), batch['pseudo_targets'][:, j]] = batch['pseudo_confidences'][:, j]
-                    pseudo_mask[torch.arange(logit.size(0)), batch['pseudo_targets'][:, j]] = 1
-                Qd = torch.softmax(pseudo_soft_label / args.tau, dim=1)
-                loss = torch.mean(-torch.sum(Qd * torch.log(Pd + 1e-10), dim=1))
+                # pseudo_mask = torch.zeros_like(logit)
+                # pseudo_mask[:, 0] = -1e10
+                # Pd = torch.softmax(logit + pseudo_mask, dim=1)
+                # pseudo_soft_label = torch.zeros_like(logit) - 1e10
+                # for j in range(batch['pseudo_targets'].size(1)):
+                #     pseudo_soft_label[torch.arange(logit.size(0)), batch['pseudo_targets'][:, j]] = batch['pseudo_confidences'][:, j]
+                #     pseudo_mask[torch.arange(logit.size(0)), batch['pseudo_targets'][:, j]] = 1
+                # Qd = torch.softmax(pseudo_soft_label / args.tau, dim=1)
+                # loss = torch.mean(-torch.sum(Qd * torch.log(Pd + 1e-10), dim=1))
 
                 ### ListNet2.0
                 # if args.stage == 'rerank':
@@ -160,15 +160,15 @@ def train_know(args, train_dataloader, test_dataloader, retriever, knowledge_dat
                 # logit = logit + pseudo_mask
 
                 ### ListMLE (final)
-                # logit_exp = torch.exp(logit - torch.max(logit, dim=1, keepdim=True)[0])  # [B, K]
-                # all_sum = torch.sum(logit_exp, dim=1, keepdim=True)  # [B, 1]
-                # pseudo_logit = torch.gather(logit_exp, 1, batch['pseudo_targets'])
-                # if args.train_ablation == 'learning2rank':
-                #     cumsum_logit = torch.cumsum(pseudo_logit, dim=1)  # [B, K]
-                #     denominator = all_sum - (cumsum_logit - pseudo_logit) + 1e-10
-                # elif args.train_ablation == 'sampling':
-                #     denominator = all_sum + 1e-10
-                # loss = torch.mean(torch.sum(-torch.log(pseudo_logit / denominator), dim=1))
+                logit_exp = torch.exp(logit - torch.max(logit, dim=1, keepdim=True)[0])  # [B, K]
+                all_sum = torch.sum(logit_exp, dim=1, keepdim=True)  # [B, 1]
+                pseudo_logit = torch.gather(logit_exp, 1, batch['pseudo_targets'])
+                if args.train_ablation == 'learning2rank':
+                    cumsum_logit = torch.cumsum(pseudo_logit, dim=1)  # [B, K]
+                    denominator = all_sum - (cumsum_logit - pseudo_logit) + 1e-10
+                elif args.train_ablation == 'sampling':
+                    denominator = all_sum + 1e-10
+                loss = torch.mean(torch.sum(-torch.log(pseudo_logit / denominator), dim=1))
 
                 ### ListMLE (sampling)
                 # logit = retriever.compute_know_score_candidate(dialog_token, dialog_mask, knowledge_index[batch['candidate_indice']])
