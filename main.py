f9c07e8551d1c34fd8b8fbd1885637da076cceb0
taeho <hirooms2@hanyang.ac.kr>
2023-05-11 20:26:14 +0900

    230421 Cand2

--- main.py
+++ main.py
@@ -241,6 +241,7 @@ def main():
             print('rerank mode')
             args.stage = 'rerank'
             # retriever.init_reranker()
+            args.num_epochs=10
             train_know(args, train_dataloader, valid_dataloader, retriever, knowledge_data, knowledgeDB, tokenizer)
         eval_know(args, test_dataloader, retriever, knowledge_data, knowledgeDB, tokenizer, write=True)  # HJ: Knowledge text top-k 뽑아서 output만들어 체크하던 코드 분리
 
