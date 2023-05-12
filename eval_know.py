b3c490565ce74106e21f0fdc670d14ec22fe2bed
taeho <hirooms2@hanyang.ac.kr>
2023-05-12 17:03:30 +0900

    230421 Cand2

--- eval_know.py
+++ eval_know.py
@@ -76,15 +76,15 @@ def eval_know(args, test_dataloader, retriever, knowledge_data, knowledgeDB, tok
         dot_score = retriever.compute_know_score(dialog_token, dialog_mask, knowledge_index, batch['type'])
 
         if args.stage == 'rerank':
-            candidate_indice = torch.topk(dot_score, k=args.know_topk, dim=1).indices  # [B, K]
+            # candidate_indice = torch.topk(dot_score, k=args.know_topk, dim=1).indices  # [B, K]
 
-            # candidate_knowledge_text = [args.knowledgeDB[idx] for candidates in candidate_indice for idx in candidates]
-            # candidate_knowledge = tokenizer(candidate_knowledge_text, truncation=True, padding='max_length', max_length=args.max_length)
-            # candidate_knowledge_token = candidate_knowledge.input_ids
-            # candidate_knowledge_mask = candidate_knowledge.attention_mask
-            # candidate_knowledge_token = torch.LongTensor(candidate_knowledge_token).to(args.device).view(-1, args.know_topk, args.max_length)
-            # candidate_knowledge_mask = torch.LongTensor(candidate_knowledge_mask).to(args.device).view(-1, args.know_topk, args.max_length)
-            # dot_score = retriever.knowledge_retrieve(dialog_token, dialog_mask, candidate_knowledge_token, candidate_knowledge_mask)  # [B, 2]
+            candidate_knowledge_text = [args.knowledgeDB[idx] for candidates in candidate_indice for idx in candidates]
+            candidate_knowledge = tokenizer(candidate_knowledge_text, truncation=True, padding='max_length', max_length=args.max_length)
+            candidate_knowledge_token = candidate_knowledge.input_ids
+            candidate_knowledge_mask = candidate_knowledge.attention_mask
+            candidate_knowledge_token = torch.LongTensor(candidate_knowledge_token).to(args.device).view(-1, args.know_topk, args.max_length)
+            candidate_knowledge_mask = torch.LongTensor(candidate_knowledge_mask).to(args.device).view(-1, args.know_topk, args.max_length)
+            dot_score = retriever.knowledge_retrieve(dialog_token, dialog_mask, candidate_knowledge_token, candidate_knowledge_mask)  # [B, 2]
 
             dot_score = retriever.compute_know_score_candidate(dialog_token, dialog_mask, knowledge_index_rerank[candidate_indice])
 
