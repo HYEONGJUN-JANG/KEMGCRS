046de206fefd6ba09047f09ed7d36498d04390d8
taeho <hirooms2@hanyang.ac.kr>
2023-05-12 13:58:57 +0900

    230421 Cand2

--- data_util.py
+++ data_util.py
@@ -159,7 +159,7 @@ def process_augment_sample(raw_data, tokenizer, knowledgeDB):
 def dataset_reader(args, data_name='train'):
     all_knowledge = set()
     conversation_sample = []
-    data_path = os.path.join(args.data_dir, f"en_{data_name}_know_cand_score.txt")
+    data_path = os.path.join(args.data_dir, f"en_{data_name}_know_cand_score20.txt")
     with open(data_path, 'r', encoding='UTF-8') as f:
         for line in tqdm(f, desc="Dataset Read", bar_format='{l_bar} | {bar:23} {r_bar}'):
             dialog = json.loads(line)
