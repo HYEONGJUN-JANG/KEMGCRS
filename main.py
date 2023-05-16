4fd6ef614d7a6380e6eb3a23077849ac609acc8c
taeho <hirooms2@hanyang.ac.kr>
2023-05-16 14:32:49 +0900

    230421 Cand2

--- main.py
+++ main.py
@@ -249,7 +249,6 @@ def main():
             for module in modules:
                 for param in module.parameters():
                     param.requires_grad = False
-            args.num_epochs = 10
             train_know(args, train_dataloader, valid_dataloader, retriever, knowledge_data, knowledgeDB, tokenizer)
             eval_know(args, test_dataloader, retriever, knowledge_data, knowledgeDB, tokenizer, write=True)  # HJ: Knowledge text top-k 뽑아서 output만들어 체크하던 코드 분리
 
