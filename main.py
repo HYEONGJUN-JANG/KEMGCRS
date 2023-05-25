180c6e72f02ce0a1822472e2b46f6066d13be243
taeho <hirooms2@hanyang.ac.kr>
2023-05-25 13:27:56 +0900

    230421 Cand2

--- main.py
+++ main.py
@@ -233,7 +233,6 @@ def main():
             args.stage = 'retrieve'
             eval_know(args, valid_dataloader, retriever, knowledge_data, knowledgeDB, tokenizer)  # HJ: Knowledge text top-k 뽑아서 output만들어 체크하던 코드 분리
             train_know(args, train_dataloader, valid_dataloader, retriever, knowledge_data, knowledgeDB, tokenizer)
-            args.stage = 'rerank'
         else:
             print('############################retriever load:\t%s#################################' % args.saved_model_path)
             retriever.load_state_dict(torch.load(os.path.join(args.model_dir, args.saved_model_path), map_location=args.device))
