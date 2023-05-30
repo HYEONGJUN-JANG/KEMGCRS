c1d05f3a7850cdb446318fd8e6fda018725d1413
taeho <hirooms2@hanyang.ac.kr>
2023-05-30 20:12:29 +0900

    230421 Cand2

--- eval_know.py
+++ eval_know.py
@@ -7,7 +7,7 @@ from tqdm import tqdm
 from data_util import bm_tokenizer
 from utils import write_pkl, save_json
 import numpy as np
-
+import pickle
 
 def knowledge_reindexing(args, knowledge_data, retriever, stage):
     # 모든 know_index를 버트에 태움
@@ -99,13 +99,15 @@ def eval_know(args, test_dataloader, retriever, knowledge_data, knowledgeDB, tok
         if retrieve:
             top_candidate = torch.topk(dot_score, k=args.know_topk, dim=1).indices  # [B, K]
             for idx in range(batch_size):
-                query_token = tokenizer.encode(tokenizer.decode(response[idx], skip_special_tokens=True) + "|" + topic_idx[idx])[1:-1]
-                bm_scores = args.bm25.get_scores(query_token)
-                candidate_list = top_candidate.cpu().numpy().tolist()[idx]
-                candidate_list_score = [bm_scores[candi] for candi in candidate_list]
-                sorted_list_idx = np.argsort(candidate_list_score)[::-1]
-                sorted_candidates = [candidate_list[idx] for idx in sorted_list_idx]
-                test_dataloader.dataset.augmented_raw_sample[current + idx]['candidate_knowledges'] = sorted_candidates
+                # query_token = tokenizer.encode(tokenizer.decode(response[idx], skip_special_tokens=True) + "|" + topic_idx[idx])[1:-1]
+                # bm_scores = args.bm25.get_scores(query_token)
+                # candidate_list = top_candidate.cpu().numpy().tolist()[idx]
+                # candidate_list_score = [bm_scores[candi] for candi in candidate_list]
+                # sorted_list_idx = np.argsort(candidate_list_score)[::-1]
+                # sorted_candidates = [candidate_list[idx] for idx in sorted_list_idx]
+                # test_dataloader.dataset.augmented_raw_sample[current + idx]['candidate_knowledges'] = sorted_candidates
+                test_dataloader.dataset.augmented_raw_sample[current + idx]['candidate_knowledges'] = top_candidate
+
             current += batch_size
 
         if write:
@@ -198,6 +200,9 @@ def eval_know(args, test_dataloader, retriever, knowledge_data, knowledgeDB, tok
     # hit_chat_result = ["%.4f" % hit for hit in hit_chat_result]
     hit_poi_result = ["%.4f" % hit for hit in hit_poi_result]
 
+    if retrieve:
+        with open('augmented_train.txt', 'wb') as f:
+            pickle.dump(test_dataloader.dataset.augmented_raw_sample, f)
     if write:
         # TODO HJ: 입출력 저장 args처리 필요시 args.save_know_output 에 store_true 옵션으로 만들 필요
         write_pkl(obj=jsonlineSave, filename='jsonline.pkl')  # 입출력 저장
--- main.py
+++ main.py
@@ -226,19 +226,23 @@ def main():
         test_datamodel_know = DialogDataset(args, test_dataset, all_knowledgeDB, tokenizer, task='know')
 
         train_dataloader = DataLoader(train_datamodel_know, batch_size=args.batch_size, shuffle=True)
+        train_dataloader_retrieve = DataLoader(train_datamodel_know, batch_size=args.batch_size, shuffle=False)
         valid_dataloader = DataLoader(test_datamodel_know, batch_size=args.batch_size, shuffle=False)
         test_dataloader = DataLoader(test_datamodel_know, batch_size=1, shuffle=False)
 
         # print('rerank mode')
         # retriever.init_reranker()
         # train_know(args, train_dataloader, valid_dataloader, retriever, knowledge_data, knowledgeDB, tokenizer)
-        # eval_know(args, test_dataloader, retriever, knowledge_data, knowledgeDB, tokenizer, write=True)  # HJ: Knowledge text top-k 뽑아서 output만들어 체크하던 코드 분리
 
         if args.saved_model_path == '':
             print('retrieve mode')
             args.stage = 'retrieve'
             eval_know(args, valid_dataloader, retriever, all_knowledge_data, all_knowledgeDB, tokenizer)  # HJ: Knowledge text top-k 뽑아서 output만들어 체크하던 코드 분리
             train_know(args, train_dataloader, valid_dataloader, retriever, train_knowledge_data, train_knowledgeDB, all_knowledge_data, all_knowledgeDB, tokenizer)
+
+            eval_know(args, train_dataloader_retrieve, retriever, train_knowledge_data, train_knowledgeDB, tokenizer, retrieve=True)  # todo: remove
+            eval_know(args, valid_dataloader, retriever, all_knowledge_data, all_knowledgeDB, tokenizer, retrieve=True)  # todo: remove
+
             args.stage = 'rerank'
         else:
             print('############################retriever load:\t%s#################################' % args.saved_model_path)
@@ -257,6 +261,7 @@ def main():
                     param.requires_grad = False
 
             args.lr = args.lr_rerank
+
             train_know(args, train_dataloader, valid_dataloader, retriever, train_knowledge_data, train_knowledgeDB, all_knowledge_data, all_knowledgeDB, tokenizer)
 
             eval_know(args, test_dataloader, retriever, all_knowledge_data, all_knowledgeDB, tokenizer, write=True)  # HJ: Knowledge text top-k 뽑아서 output만들어 체크하던 코드 분리
