fad5fd9862f721a9c257f1443bfb5f76346ce103
taeho <hirooms2@hanyang.ac.kr>
2023-06-21 22:59:32 +0900

    230421 Cand2

--- main.py
+++ main.py
@@ -222,10 +222,10 @@ def main():
                     print("[%s]\t%.4f" % (goal_type, hitDic[goal_type]['hit1'] / hitDic[goal_type]['count']))
                 print("[All]\t%.4f" % (hit_all / hitDic[goal_type]['count']))
 
-                with open(f"response_write_{args.time}_{args.model_name}_{args.gpt_name}_{args.lr}_{epoch}.txt", 'w', encoding='UTF-8') as f:
-                    for (a, b, c) in zip(all_dialog, all_response, all_generated):
-                        f.write('[DIALOG]\t%s\n[RESPONSE]\t%s\n[GENERATED]\t%s\n' % (a, b, c))
-                        f.write('-------------------------------------------\n')
+                # with open(f"response_write_{args.time}_{args.model_name}_{args.gpt_name}_{args.lr}_{epoch}.txt", 'w', encoding='UTF-8') as f:
+                #     for (a, b, c) in zip(all_dialog, all_response, all_generated):
+                #         f.write('[DIALOG]\t%s\n[RESPONSE]\t%s\n[GENERATED]\t%s\n' % (a, b, c))
+                #         f.write('-------------------------------------------\n')
         else:
             generator.load_state_dict(torch.load(os.path.join(args.model_dir, args.saved_model_path)))
 
