fa10c4f593e8cd26da75a8562c0292209bce074f
taeho <hirooms2@hanyang.ac.kr>
2023-07-03 16:24:45 +0900

    230421 Cand2

--- main.py
+++ main.py
@@ -131,6 +131,8 @@ def main():
     args.all_knowledge_num = len(all_knowledgeDB)
     args.all_knowledgeDB = all_knowledgeDB
 
+    # args.saved_model_path = 'myretriever_resp'
+
     if 'goal' in args.task:
         # KNOWLEDGE TASk
         retriever = Retriever(args, bert_model)
@@ -170,6 +172,7 @@ def main():
 
         train_dataset_resp = process_augment_sample_topic(train_dataset_raw, tokenizer, train_knowledgeDB)
         test_dataset_resp = process_augment_sample_topic(test_dataset_raw, tokenizer, all_knowledgeDB)
+        # test_dataset_resp_retrieve = process_augment_sample(test_dataset_raw, tokenizer, all_knowledgeDB)
 
         # with open('augmented_dataset_test.txt', 'rb') as f:
         #     test_dataset_resp = pickle.load(f)
@@ -180,106 +183,105 @@ def main():
 
         train_datamodel_resp = GenerationDataset(args, train_dataset_resp, train_knowledgeDB, tokenizer, mode='train', subtask=args.subtask)
         test_datamodel_resp = GenerationDataset(args, test_dataset_resp, all_knowledgeDB, tokenizer, mode='test', subtask=args.subtask)
+        # test_datamodel_resp_retrieve = GenerationDataset(args, test_dataset_resp_retrieve, all_knowledgeDB, tokenizer, mode='test', subtask=args.subtask)
 
         train_dataloader_resp = DataLoader(train_datamodel_resp, batch_size=args.batch_size, shuffle=True)
         test_dataloader_resp = DataLoader(test_datamodel_resp, batch_size=args.batch_size, shuffle=False)
+        # test_dataloader_resp_retrieve = DataLoader(test_datamodel_resp_retrieve, batch_size=args.batch_size, shuffle=False)
 
         generator = Retriever(args, gpt_model=gpt_model)
         generator = generator.to(args.device)
-        criterion = nn.CrossEntropyLoss().to(args.device)
+        criterion = nn.CrossEntropyLoss()
         optimizer = optim.AdamW(generator.parameters(), lr=args.lr)
 
         # train generate task
-        if args.saved_model_path == '':
-            best_hit = 0
-            for epoch in range(args.num_epochs):
-                train_epoch_loss = 0
-                for batch in tqdm(train_dataloader_resp, desc="Generate_Train", bar_format=' {l_bar} | {bar:23} {r_bar}'):
-                    generator.train()
-                    dialog_token = batch['input_ids'].to(args.device)
-                    dialog_mask = batch['attention_mask'].to(args.device)
-                    response = batch['response'].to(args.device)
-                    topic_idx = batch['topic_idx'].to(args.device)
+        if args.saved_model_path != '':
+            generator.load_state_dict(torch.load(os.path.join(args.model_dir, f"{args.saved_model_path}_goal_best.pt")))
 
-                    loss = generator.generation(dialog_token, dialog_mask, response, topic_idx)
-                    # loss = criterion(dot_score, targets)
-                    train_epoch_loss += loss
-                    optimizer.zero_grad()
-                    loss.backward()
-                    optimizer.step()
-                print(f"Epoch: {epoch}\nTrain Loss: {train_epoch_loss}")
+        best_hit = 0
+        for epoch in range(args.num_epochs):
+            # train_epoch_loss = 0
+            # for batch in tqdm(train_dataloader_resp, desc="Generate_Train", bar_format=' {l_bar} | {bar:23} {r_bar}'):
+            #     generator.train()
+            #     dialog_token = batch['input_ids'].to(args.device)
+            #     dialog_mask = batch['attention_mask'].to(args.device)
+            #     response = batch['response'].to(args.device)
+            #     topic_idx = batch['topic_idx'].to(args.device)
+            #
+            #     loss = generator.generation(dialog_token, dialog_mask, response, topic_idx)
+            #     # loss = criterion(dot_score, targets)
+            #     train_epoch_loss += loss
+            #     optimizer.zero_grad()
+            #     loss.backward()
+            #     optimizer.step()
+            # print(f"Epoch: {epoch}\nTrain Loss: {train_epoch_loss}")
 
-                # test generation task
-                all_dialog = []
-                all_response = []
-                all_generated = []
-                goal_types = []
-                for batch in tqdm(test_dataloader_resp, desc="Generate Test", bar_format=' {l_bar} | {bar:23} {r_bar}'):
-                    generator.eval()
-                    dialog_token = batch['input_ids'].to(args.device)
-                    dialog_mask = batch['attention_mask'].to(args.device)
-                    response = batch['response']
+            # test generation task
+            all_dialog = []
+            all_response = []
+            all_generated = []
+            goal_types = []
+            for batch in tqdm(test_dataloader_resp, desc="Generate Test", bar_format=' {l_bar} | {bar:23} {r_bar}'):
+                generator.eval()
+                dialog_token = batch['input_ids'].to(args.device)
+                dialog_mask = batch['attention_mask'].to(args.device)
+                response = batch['response']
 
-                    batch_size = dialog_token.shape[0]
-                    generated = generator.gpt_model.generate(input_ids=dialog_token,
-                                                             attention_mask=dialog_mask,
-                                                             pad_token_id=tokenizer.pad_token_id,
-                                                             max_length=args.max_gen_length)
-                    # decoded_generated = tokenizer.batch_decode(generated)
+                batch_size = dialog_token.shape[0]
+                generated = generator.gpt_model.generate(input_ids=dialog_token,
+                                                         attention_mask=dialog_mask,
+                                                         pad_token_id=tokenizer.pad_token_id,
+                                                         max_length=args.max_gen_length)
+                # decoded_generated = tokenizer.batch_decode(generated)
 
-                    gen_resp_ids = []
-                    for gen_seq, length in zip(generated, batch['context_len']):
-                        gen_seq = [token_id for token_id in gen_seq if token_id != tokenizer.pad_token_id]
-                        # gen_resp_ids.append(gen_seq[length:]) # for GPT
-                        gen_resp_ids.append(gen_seq)
+                gen_resp_ids = []
+                for gen_seq, length in zip(generated, batch['context_len']):
+                    gen_seq = [token_id for token_id in gen_seq if token_id != tokenizer.pad_token_id]
+                    # gen_resp_ids.append(gen_seq[length:]) # for GPT
+                    gen_resp_ids.append(gen_seq)
 
-                    all_generated.extend(tokenizer.batch_decode(gen_resp_ids, skip_special_tokens=True))
-                    all_response.extend(tokenizer.batch_decode(response, skip_special_tokens=True))
-                    all_dialog.extend(tokenizer.batch_decode(dialog_token, skip_special_tokens=True))
-                    goal_types.extend(tokenizer.batch_decode(batch['goal_type'], skip_special_tokens=True))
+                all_generated.extend(tokenizer.batch_decode(gen_resp_ids, skip_special_tokens=True))
+                all_response.extend(tokenizer.batch_decode(response, skip_special_tokens=True))
+                all_dialog.extend(tokenizer.batch_decode(dialog_token, skip_special_tokens=True))
+                goal_types.extend(tokenizer.batch_decode(batch['goal_type'], skip_special_tokens=True))
 
-                with open(f"response_write_{args.time}_{args.model_name}_{args.gpt_name}_{args.lr}_{epoch}.txt", 'w', encoding='UTF-8') as f:
-                    for (a, b, c) in zip(all_dialog, all_response, all_generated):
-                        f.write('[DIALOG]\t%s\n[RESPONSE]\t%s\n[GENERATED]\t%s\n' % (a, b, c))
-                        f.write('-------------------------------------------\n')
+            with open(f"response_write_{args.time}_{args.model_name}_{args.gpt_name}_{args.lr}_{epoch}.txt", 'w', encoding='UTF-8') as f:
+                for (a, b, c) in zip(all_dialog, all_response, all_generated):
+                    f.write('[DIALOG]\t%s\n[RESPONSE]\t%s\n[GENERATED]\t%s\n' % (a, b, c))
+                    f.write('-------------------------------------------\n')
 
-                typelist = ['Q&A', 'POI recommendation', 'Movie recommendation', 'Music recommendation']
-                # typelist=['Q&A'] if args.onlyQA else
-                hitDic = {type: {'hit1': [], 'hit3': [], 'hit5': []} for type in typelist}
-                hitAll = {'hit1': [], 'hit3': [], 'hit5': []}
-                hit_list = [1]
-                for idx in range(len(all_generated)):
-                    gold = all_response[idx]
-                    pred = all_generated[idx]
-                    goal_type = goal_types[idx]
+            typelist = ['Q&A', 'POI recommendation', 'Movie recommendation', 'Music recommendation']
+            # typelist=['Q&A'] if args.onlyQA else
+            hitDic = {type: {'hit1': [], 'hit3': [], 'hit5': []} for type in typelist}
+            hitAll = {'hit1': [], 'hit3': [], 'hit5': []}
+            hit_list = [1]
+            for idx in range(len(all_generated)):
+                gold = all_response[idx]
+                pred = all_generated[idx]
+                goal_type = goal_types[idx]
 
-                    correct = (gold == pred)
-                    hitAll["hit1"].append(correct)
-                    # hitDic[goal_type]["hit1"].append(correct)
+                correct = (gold == pred)
+                hitAll["hit1"].append(correct)
+                # hitDic[goal_type]["hit1"].append(correct)
 
-                    # pred = pred.split('|')
-                    #
-                    # for k in hit_list:
-                    #     correct = gold in pred[:k + 1]
-                    #     hitAll[f"hit{k}"].append(correct)
-                    #     hitDic[goal_type][f"hit{k}"].append(correct)
-
-                    # total_cnt=sum([hitDic[type]['hit1'] for type in typelist])
-                    # hitDic['total_hit1_ratio'] = round(sum([hitDic[type]['hit1'] for type in typelist ]) / total_cnt,3)
-
+                # pred = pred.split('|')
+                #
                 # for k in hit_list:
-                #     for goal_type in typelist:
-                #         print("[hit%d]\t[%s]\t%.4f" % (k, goal_type, np.average(hitDic[goal_type][f"hit{k}"])))
-                print("[hit1]\t[%s]\t%.4f" % (np.average(hitAll[f"hit1"]), args.subtask))
-                if best_hit < np.average(hitAll[f"hit1"]):
-                    best_hit = np.average(hitAll[f"hit1"])
-                    torch.save(generator.state_dict(), os.path.join(args.model_dir, f"{args.model_name}_{args.task}_{args.subtask}_{args.num_epochs}.pt"))  # TIME_MODELNAME 형식
+                #     correct = gold in pred[:k + 1]
+                #     hitAll[f"hit{k}"].append(correct)
+                #     hitDic[goal_type][f"hit{k}"].append(correct)
 
-            print("[BEST][hit1]\t[%s]\t%.4f" % (best_hit, args.subtask))
+                # total_cnt=sum([hitDic[type]['hit1'] for type in typelist])
+                # hitDic['total_hit1_ratio'] = round(sum([hitDic[type]['hit1'] for type in typelist ]) / total_cnt,3)
 
-
-        else:
-            generator.load_state_dict(torch.load(os.path.join(args.model_dir, args.saved_model_path)))
+            # for k in hit_list:
+            #     for goal_type in typelist:
+            #         print("[hit%d]\t[%s]\t%.4f" % (k, goal_type, np.average(hitDic[goal_type][f"hit{k}"])))
+            print("[hit1]\t[%s]\t%.4f" % (np.average(hitAll[f"hit1"]), args.subtask))
+            if best_hit < np.average(hitAll[f"hit1"]):
+                best_hit = np.average(hitAll[f"hit1"])
+                torch.save(generator.state_dict(), os.path.join(args.model_dir, f"{args.model_name}_{args.task}_{args.subtask}_{args.num_epochs}.pt"))  # TIME_MODELNAME 형식
+        print("[BEST][hit1]\t[%s]\t%.4f" % (best_hit, args.subtask))
 
     if 'topic' in args.task:
         # KNOWLEDGE TASk
