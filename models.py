6c301600787b9862e8bc8ce025d853fce985e595
taeho <hirooms2@hanyang.ac.kr>
2023-07-05 01:24:27 +0900

    230421 Cand2

--- models.py
+++ models.py
@@ -134,7 +134,6 @@ class Retriever(nn.Module):
         # logit_inbatch = torch.sum(dialog_emb.unsqueeze(1) * knowledge_index_pos, dim=2)  # [B, B]
 
         logit_pos = torch.sum(dialog_emb.unsqueeze(1) * knowledge_index_pos, dim=2)  # [B, 1, d] * [B, K, d] = [B, K]
-
         logit_neg = torch.matmul(dialog_emb, knowledge_index_neg.squeeze(1).transpose(1, 0))  # [B, B]
         # logit_mask = torch.zeros_like(logit_neg).fill_diagonal_(-1e10)
         # logit_neg = logit_neg + logit_mask  # [B, B]
