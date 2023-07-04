2f85a8d269563ddab8411939d4fbdaddf879d37c
taeho <hirooms2@hanyang.ac.kr>
2023-07-05 01:50:34 +0900

    230421 Cand2

--- train_know.py
+++ train_know.py
@@ -236,7 +236,7 @@ def train_know(args, train_dataloader, test_dataloader, retriever, knowledge_dat
                 # logit = retriever.compute_know_score_candidate(dialog_token, dialog_mask, knowledge_index[batch['candidate_indice']])
 
                 if args.stage == 'rerank':
-                    loss = retriever.dpr_retrieve_train(dialog_token, dialog_mask, candidate_knowledge_token, candidate_knowledge_mask)
+                    # loss = retriever.dpr_retrieve_train(dialog_token, dialog_mask, candidate_knowledge_token, candidate_knowledge_mask)
 
                     # logit_pos, logit_neg = retriever.knowledge_retrieve(dialog_token, dialog_mask, candidate_indice, candidate_knowledge_token, candidate_knowledge_mask)  # [B, 2]
                     # logit = torch.cat([logit_pos, logit_neg], dim=-1)
@@ -250,6 +250,13 @@ def train_know(args, train_dataloader, test_dataloader, retriever, knowledge_dat
                     #     pseudo_mask = torch.cat([torch.zeros(pseudo_mask.size(0)).unsqueeze(1).to(args.device), pseudo_mask], dim=1)
                     #     loss += (-torch.log_softmax(g_logit+pseudo_mask, dim=1).select(dim=1, index=0)).mean()
 
+                    logit_pos, logit_neg = retriever.knowledge_retrieve(dialog_token, dialog_mask, candidate_indice, candidate_knowledge_token, candidate_knowledge_mask)  # [B, 2]
+                    cumsum_logit = torch.cumsum(logit_pos, dim=1)  # [B, K]
+                    loss = 0
+                    for idx in range(args.pseudo_pos_rank):
+                        g_logit = cumsum_logit[:, idx] / (idx + 1)
+                        g_logit = torch.cat([g_logit.unsqueeze(1), logit_neg], dim=1)
+                        loss += (-torch.log_softmax(g_logit, dim=1).select(dim=1, index=0)).mean()
 
                 ### Group-wise + Seq (original)
                 # loss = torch.mean(criterion(logit, batch['pseudo_targets'][:, 0]))
