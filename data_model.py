16197c3564b00419d2bb9ab3529855e1daba8091
taeho <hirooms2@hanyang.ac.kr>
2023-07-05 13:43:09 +0900

    230421 Cand2

--- data_model.py
+++ data_model.py
@@ -273,7 +273,7 @@ class DialogDataset(Dataset):  # knowledge용 데이터셋
                                                    truncation=True).input_ids
 
         context_batch['goal'] = self.args.goalDic['str'][goal]  # index로 바꿈
-        context_batch['topic_idx'] = self.args.topicDic['str'][topic]  # index로 바꿈
+        # context_batch['topic_idx'] = self.args.topicDic['str'][topic]  # index로 바꿈
         context_batch['topic'] = self.tokenizer(topic, truncation=True, padding='max_length', max_length=32).input_ids
 
         # List-wise
