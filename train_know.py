8e2e3640b4d6f16c4f8b5be615dcd7bafb2eaefe
taeho <hirooms2@hanyang.ac.kr>
2023-07-08 01:41:51 +0900

    230421 Cand2

--- train_know.py
+++ train_know.py
@@ -256,30 +256,21 @@ def train_know(args, train_dataloader, test_dataloader, retriever, knowledge_dat
 
                     # GCL version (+food의 경우에는 idea X)
 
-                    # logit_pos, logit_neg = retriever.knowledge_retrieve(dialog_token, dialog_mask, candidate_indice, candidate_knowledge_token, candidate_knowledge_mask)  # [B, 2]
-                    # #     loss += (-torch.log_softmax(g_logit + pseudo_mask, dim=1).select(dim=1, index=0)).mean()
-                    # isFood = batch['isFood'].view(logit_pos.size(0), -1).repeat(1, args.pseudo_pos_rank).long()
-                    # isFood = torch.ones_like(isFood) - isFood
-                    # isFood[:, 0] = 1
-                    # batch_denominator = torch.cumsum(isFood, dim=-1)  # [B]
-                    # # isFood = isFood * -1e10
-                    #
-                    # cumsum_logit = torch.cumsum(logit_pos * isFood, dim=1)  # [B, K]  # Grouping
-                    # # num_samples = torch.cumsum(sampling_results, dim=-1)
-                    # # cumsum_logit = logit_pos  # torch.cumsum(logit_pos, dim=1)  # [B, K]  # For Sampling
-                    #
-                    # loss = 0
-                    # # pseudo_confidences = batch['pseudo_confidences']
-                    # for idx in range(args.pseudo_pos_rank):
-                    #     # confidence = torch.softmax(pseudo_confidences[:, :idx + 1], dim=-1)
-                    #     # g_logit = torch.sum(logit_pos[:, :idx + 1] * confidence, dim=-1)
-                    #     # g_logit = cumsum_logit[:, idx] / (idx + 1)
-                    #     g_logit = cumsum_logit[:, idx] / batch_denominator[:, idx]
-                    #
-                    #     # g_logit = cumsum_logit[:, idx] / num_samples[:, idx]
-                    #     # g_logit = cumsum_logit[:, idx]  # For Sampling
-                    #     g_logit = torch.cat([g_logit.unsqueeze(1), logit_neg], dim=1)
-                    #     loss += (-torch.log_softmax(g_logit, dim=1).select(dim=1, index=0)).mean()
+                    logit_pos, logit_neg = retriever.knowledge_retrieve(dialog_token, dialog_mask, candidate_indice, candidate_knowledge_token, candidate_knowledge_mask)  # [B, 2]
+                    cumsum_logit = torch.cumsum(logit_pos, dim=1)  # [B, K]  # Grouping
+
+                    loss = 0
+                    # pseudo_confidences = batch['pseudo_confidences']
+                    for idx in range(args.pseudo_pos_rank):
+                        # confidence = torch.softmax(pseudo_confidences[:, :idx + 1], dim=-1)
+                        # g_logit = torch.sum(logit_pos[:, :idx + 1] * confidence, dim=-1)
+                        g_logit = cumsum_logit[:, idx] / (idx + 1)
+                        # g_logit = cumsum_logit[:, idx] / batch_denominator[:, idx]
+
+                        # g_logit = cumsum_logit[:, idx] / num_samples[:, idx]
+                        # g_logit = cumsum_logit[:, idx]  # For Sampling
+                        g_logit = torch.cat([g_logit.unsqueeze(1), logit_neg], dim=1)
+                        loss += (-torch.log_softmax(g_logit, dim=1).select(dim=1, index=0)).mean()
 
                 ### Group-wise + Seq (original)
                 # loss = torch.mean(criterion(logit, batch['pseudo_targets'][:, 0]))
